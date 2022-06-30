2ecc64ed2e23abf70f15747a9e79340863abadc1
Gavin Stark <gavin@gstark.com>
2022-06-29 21:50:25 -0400

    Adjustment

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-# gotenberg --api-port 4000 --log-level $LOG_LEVEL &
+gotenberg --api-port 4000 --log-level $LOG_LEVEL &
 envsubst '\$PORT' < /home/gotenberg/nginx.subst.conf > /home/gotenberg/nginx.conf
 # cat /home/gotenberg/nginx.conf
 nginx -c /home/gotenberg/nginx.conf
